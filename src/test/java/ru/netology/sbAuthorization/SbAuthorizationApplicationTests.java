76c726451069567f794cdffa3535a4b48e81e7a1
ksa237 <ksa237@yandex.ru>
2026-01-16 22:20:39 +1000

    init commit

---
+++ src/test/java/ru/netology/sbAuthorization/SbAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.sbAuthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
